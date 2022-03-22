ead8d93bfc24638ed51f74f647ffa4347d31bcb7
Samuel <369282275@qq.com>
2022-03-22 22:27:20 +0800

    提交

--- Project1/EulerTimer.h
+++ Project1/EulerTimer.h
@@ -1,4 +1,6 @@
-#include<MMSystem.h>
+#include<windows.h>
+#include<mmsystem.h>
+//多媒体相关API
 #include"EulerSystem.h"
 namespace EulerEngine {
 	class EulerTimer {
@@ -9,6 +11,7 @@ namespace EulerEngine {
 		double LastTime;
 		double FrameCount;
 		double TimeStart;
+		double LastFPSTime;
 
 		void InitGameTime() {
 			FPS = 0;
@@ -16,12 +19,24 @@ namespace EulerEngine {
 			DeltaTime = 0;
 			LastTime = 0;
 			FrameCount = 0;
+			LastFPSTime = 0;
 			TimeStart = timeGetTime();
 		}
-		void GetGamePlayTime() {
-			
+		double GetGamePlayTime() {
+			return (timeGetTime() - TimeStart);
 		}
 		void UpdateFps() {
+			Time = this->GetGamePlayTime()*0.001;
+			DeltaTime = Time - LastTime;
+			LastTime = Time;
+			if (Time - LastFPSTime > 1.0f) {
+				LastFPSTime = Time;
+				FPS = FrameCount;
+				FrameCount = 0;
+			}
+			else {
+				FrameCount++;
+			}
 		}
 	};
 }
