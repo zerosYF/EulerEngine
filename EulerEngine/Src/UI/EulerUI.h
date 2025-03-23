c77a1d3b8aa3a2c0424fa3426fc020038912a452
周宇峰 <369282275@qq.com>
2025-03-24 02:46:39 +0800

    ui小件

--- EulerEngine/Src/UI/EulerUI.h
+++ EulerEngine/Src/UI/EulerUI.h
@@ -1,4 +1,24 @@
 #pragma once
+#include"../ImGui/imgui.h"
 namespace EulerEngine {
-	
+	struct ScopedStyleColor {
+		ScopedStyleColor() = default;
+		ScopedStyleColor(ImGuiCol idx, ImVec4 color, bool predicate=true):m_Set(predicate) {
+			if (predicate) {
+				ImGui::PushStyleColor(idx, color);
+			}
+		}
+		ScopedStyleColor(ImGuiCol idx, ImU32 color, bool predicate = true) :m_Set(predicate) {
+			if (predicate) {
+				ImGui::PushStyleColor(idx, color);
+			}
+		}
+		~ScopedStyleColor() {
+			if (m_Set) {
+				ImGui::PopStyleColor();
+			}
+		}
+	private:
+		bool m_Set;
+	};
 }
